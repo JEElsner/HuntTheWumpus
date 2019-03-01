923f1d1f722fe7b6d54493c5b74413f8a7b6b8ef
Razvan Constantinescu <sn390603@kent.k12.wa.us>
2019-03-01 15:22:55 -0800

    Cave and Trivia
    Basic implementation, empty methods

---
+++ src/wumpus/Cave.java
@@ -0,0 +1,31 @@
+package wumpus;
+import java.util.*;
+
+	public class Cave{
+
+		public Cave()
+		{
+		}
+
+		public int getOne()
+		{ 
+		return 0;
+		}
+
+		public int getTwo()
+		{ 
+		return 0;
+		}
+
+		public int getThree()
+		{ 
+		return 0;
+		}
+
+		public boolean checkRoom()
+		{
+		return false;
+		}
+
+	}
+

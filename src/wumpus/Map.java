625bc4dc0e55aa94c5d0421f860c214c7431cbb8
Brandon Conner <sn321867@kent.k12.wa.us>
2019-04-09 15:39:40 -0700

    Filled in Methods
    I filled most of my pre-stubbed methods, and finished hardcoding the
    neighbors

--- src/wumpus/Map.java
+++ src/wumpus/Map.java
@@ -3,13 +3,19 @@ package wumpus;
 
 public class Map
 {
-	public int StartingRoom;
-	public int PlayerRoom;
+	public int StartingRoom = 1;
+	public int PlayerRoom = 1;
 	public int WumpusRoom;
 	public int PitRoom;
 	public int PitRoom2;
 	public int BatRoom;
 	public int BatRoom2;
+	
+	public Map()
+	{
+		
+	}
+	
 	
 	public static final int [][] neighbors = new int[30][]; // The 30 represents the room number, and the 0 underneath is the room number - 1 due to index shenanigans
 	
@@ -21,7 +27,7 @@ public class Map
 		//The third number is DOWN_RIGHT 
 		//The fourth number is DOWN
 		//The fifth number is DOWN_LEFT
-		//The sixth number is UP
+		//The sixth number is UP_LEFT
 		neighbors[1] = new int[] {26, 3, 9, 8, 7, 1};
 		neighbors[2] = new int[] {27, 28, 4, 9, 2, 26};
 		neighbors[3] = new int[] {28, 5, 11, 10, 9, 3};
@@ -32,72 +38,131 @@ public class Map
 		neighbors[8] = new int[] {3, 4, 10, 15, 8, 2};
 		neighbors[9] = new int[] {4, 11, 17, 16, 15, 9};
 		neighbors[10] = new int[] {5, 6, 12, 17, 10, 4};
-		neighbors[11] = new int[] {6,7,13,18,17,11,};
-		neighbors[12] = new int[] {13,};
-		neighbors[13] = new int[] {};
-		neighbors[14] = new int[] {};
-		neighbors[15] = new int[] {};
-		neighbors[16] = new int[] {};
-		neighbors[17] = new int[] {};
-		neighbors[18] = new int[] {};
-		neighbors[19] = new int[] {};
-		neighbors[20] = new int[] {};
-		neighbors[21] = new int[] {};
-		neighbors[22] = new int[] {};
-		neighbors[23] = new int[] {};
-		neighbors[24] = new int[] {};
-		neighbors[25] = new int[] {};
-		neighbors[26] = new int[] {};
-		neighbors[27] = new int[] {};
-		neighbors[28] = new int[] {};
-		neighbors[29] = new int[] {};
+		neighbors[11] = new int[] {6, 7, 13, 18, 17, 11,};
+		neighbors[12] = new int[] {7, 8, 14, 19, 18, 12,};
+		neighbors[13] = new int[] {8, 15, 21, 20, 19, 13};
+		neighbors[14] = new int[] {9, 10, 16, 21, 14, 8};
+		neighbors[15] = new int[] {10, 17, 23, 22, 21, 15};
+		neighbors[16] = new int[] {11, 12, 18, 23, 16, 10};
+		neighbors[17] = new int[] {12, 13, 19, 24, 23, 17};
+		neighbors[18] = new int[] {13, 14, 20, 25, 24, 18};
+		neighbors[19] = new int[] {14, 21, 27, 26, 25, 19};
+		neighbors[20] = new int[] {15, 16, 22, 27, 20, 14};
+		neighbors[21] = new int[] {16, 23, 29, 28, 27, 21};
+		neighbors[22] = new int[] {17, 18, 24, 29, 22, 16};
+		neighbors[23] = new int[] {18, 19, 25, 30, 29, 23};
+		neighbors[24] = new int[] {19, 20, 26, 1, 30, 24};
+		neighbors[25] = new int[] {20, 27, 3, 2, 1, 25};
+		neighbors[26] = new int[] {21, 22, 28, 3, 26, 20};
+		neighbors[27] = new int[] {22, 29, 5, 4, 3, 27};
+		neighbors[28] = new int[] {23, 24, 30, 5, 28, 22};
+		neighbors[29] = new int[] {24, 25, 1, 6, 5, 29};
 	}
 	
 	public static void debug()
 	{
-		Map map = new Map();
-		map.PlayerRoom = 5;
-		map.WumpusRoom = 6;
-		for(int i = 0; i < 6; i ++)
+		int x;
+		Map map = new Map(); 
+		
+		/* for(int init = 2; init <= 6; init += 2)
 		{
-			if (neighbors[4][i] == map.WumpusRoom)
-				System.out.println("There is a Wumpus in room: " + neighbors[map.PlayerRoom][i]);
-			else
-				System.out.println("No Wumpus");
+			x = init;
+			
+			do 
+			{
+				System.out.println(x);
+				x = map.getNearbyRoom(x, MovementDirection.UP);	
+			}while(x != init);
 		}
+		*/
+		map.PitRoom = 16;
+		map.PitRoom2 = 24;
+		map.PlayerRoom = 23;
+		x = map.CheckForPits();
+		System.out.println(x);
+		
 	}
 	
 	public int getWumpusRoom()
 	{
-		return 0;
+		return WumpusRoom;
 	}
 	
-	public boolean ShootArrow(MovementDirection d)
+	public int getPlayerRoom()
 	{
-		// MovementDirection d will be whatever the player inputs on the keyboard for where he wants to shoot the arrow
-		return false;
-		
+		return PlayerRoom;
+	}
+	
+	public int movePlayer(MovementDirection dir)
+	{
+		PlayerRoom = getNearbyRoom(PlayerRoom, dir);
+		return PlayerRoom;
+	}
+	
+	public int getNearbyRoom(int room, MovementDirection dir)
+	{
+		int newRoom = neighbors[room - 1][dir.ordinal()];
+		return newRoom;
+	}
+	
+	public boolean ShootArrow(MovementDirection dir)
+	{
+		if(WumpusRoom == getNearbyRoom(PlayerRoom, dir))
+			return true;
+		return false;		
 	}
 	
 	public int flyAway()
 	{
-		// if player room is the same as the room as the bat
-		// random number generator that returns a room number between 1 and 30
-		// check to make sure the new randomly generated room number isn't the same as the current room
-		// returns new room number
-		return 0;
+		if(PlayerRoom == BatRoom)
+		{
+			int same = PlayerRoom;
+			while(PlayerRoom == same)
+				PlayerRoom = (int) (Math.random() * 30 + 1);
+			int same2 = BatRoom;
+			while(BatRoom == same)
+				BatRoom = (int) (Math.random() * 30 + 1);
+		}
+		
+		else if(PlayerRoom == BatRoom2)
+		{
+			int same = PlayerRoom;
+			while(PlayerRoom == same)
+				PlayerRoom = (int) (Math.random() * 30 + 1);
+			int same2 = BatRoom2;
+			while(BatRoom2 == same2)
+				BatRoom2 = (int) (Math.random() * 30 + 1);
+		}
+		return PlayerRoom;
 	}
 	
-	public boolean BatWarning()
+	public int CheckForBats()
 	{
-		return false;
-		//if the player has a neighbor room containing a bat, then it will send true to game control, and therefore the GUI in order to send a warning
+		int count = 0;
+		for(MovementDirection dir : MovementDirection.values())
+		{
+			if(BatRoom == getNearbyRoom(PlayerRoom, dir))
+				count++;
+			if(BatRoom2 == getNearbyRoom(PlayerRoom, dir))
+				count++;
+		}
+			
+		return count;
+		//if the player has a neighbor room containing a bat, then it will the number of adjacent bats, and therefore the GUI in order to send a warning
 	}
 	
-	public boolean PitWarning()
+	public int CheckForPits()
 	{
-		return false;
-		//if the player has a neighbor room containing a bat, then it will send true to game control, and therefore the GUI in order to send a warning
+		int count = 0;
+		for(MovementDirection dir : MovementDirection.values())
+		{
+			if(PitRoom == getNearbyRoom(PlayerRoom, dir))
+				count++;
+			if(PitRoom2 == getNearbyRoom(PlayerRoom, dir))
+				count++;
+		}
+		return count;
+		//if the player has a neighbor room containing a bat, then it will send the number of adjacent pits, and therefore the GUI in order to send a warning
 	}
 	
 	public boolean fallIntoPit()

12c8ddc869654cd8f42198ed491ef9dd0957ed45
Jonathan Elsner <sn323583@kent.k12.wa.us>
2019-04-26 18:57:22 -0700

    Add ARROW_MISS value & Describe other values

--- src/gui/UpdateType.java
+++ src/gui/UpdateType.java
@@ -18,7 +18,7 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
+	 * Purpose: IDK yet
 	 * Objects:
 	 */
 	NEW_GAME,
@@ -30,8 +30,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI to display high scores
+	 * Objects: Array of high scores
 	 */
 	GET_HIGH_SCORE,
 	
@@ -42,8 +42,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify GUI to display trivia screen
+	 * Objects: String trivia question
 	 */
 	GET_TRIVIA,
 	
@@ -55,8 +55,10 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
+	 * Purpose: IDK
 	 * Objects:
+	 * 
+	 * This may not be necessary
 	 */
 	CHECK_ENCOUNTER,
 
@@ -82,8 +84,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI to display the win screen
+	 * Objects: int Score of the player
 	 */
 	DISPLAY_WIN,
 	
@@ -94,8 +96,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI to display the lose screen
+	 * Objects: int Score of the player
 	 */
 	DISPLAY_LOSE,
 	
@@ -106,8 +108,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI to tell user they've encountered a bat
+	 * Objects: String trivia question
 	 */
 	ENCOUNTER_BAT,
 	
@@ -118,8 +120,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI to tell user they've encountered a pit
+	 * Objects: String Trivia question
 	 */
 	ENCOUNTER_PIT,
 	
@@ -130,8 +132,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI to tell user they've encountered the Wumpus
+	 * Objects: String trivia question
 	 */
 	ENCOUNTER_WUMPUS,
 	
@@ -143,8 +145,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the gui it is the player's turn to move
+	 * Objects: N/A
 	 */
 	MOVE, // The user requested the player move, the data should be the MovementDirection enum
 	
@@ -155,8 +157,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI of how many arrows the player has
+	 * Objects: int number of arrows
 	 */
 	PURCHASE_ARROW, // The user requested an arrow is purchased
 	
@@ -167,8 +169,8 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI the user has purchased a secret
+	 * Objects: String secret
 	 */
 	PURCHASE_SECRET, // The user requested a secret is purchased
 	
@@ -179,10 +181,15 @@ public enum UpdateType
 	 * 
 	 * To GUI:
 	 * ------------
-	 * Purpose:
-	 * Objects:
+	 * Purpose: Notify the GUI the user has shot an arrow
+	 * Objects: int number of arrows the user has
 	 */
 	SHOOT_ARROW, // The user requested an arrow is shot, the data should be the MovementDirection enum
+	
+	/* To GUI: Notify the GUI the arrow did not hit anything
+	 * Objects: N/A?
+	 */
+	ARROW_MISS
 	
 	;
 }

240de1b9678ad00f0329fd410132b27bc4a4797b
Brandon Conner <sn321867@kent.k12.wa.us>
2019-06-01 13:58:52 -0700

    Made a new debug method for the cheat map

--- src/wumpus/Map.java
+++ src/wumpus/Map.java
@@ -1,6 +1,7 @@
 // Brandon Conner
 package wumpus;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class Map
@@ -128,7 +129,37 @@ public class Map
 
 	public static void debug()
 	{
-		// Map m1 = new Map();
+		try
+		{
+			Field controlF = Control.class.getDeclaredField("controlObject");
+			controlF.setAccessible(true);
+			
+			Field mapF = Control.class.getDeclaredField("mapObject");
+			mapF.setAccessible(true);
+			
+			Control controller = (Control) controlF.get(null);
+			Map map = (Map) mapF.get(controller);
+			
+			map.setDefaultMap();
+			
+		} catch (NoSuchFieldException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void setDefaultMap()
